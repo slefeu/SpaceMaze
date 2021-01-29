2c93ac2ad284ca6d83fa7c028a4348c715a6fcf4
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-01-30 00:21:47 +0100

    création du projet et de ma scene de travail

---
+++ BRIMMSSS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    //il faut que les énigmes soit toutes finis pour sortir du labyrinthe
+    public int puzzleToDo=4;
+    public int puzzleDone=0;
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puzzleDone == puzzleToDo)
+        {
+            //
+        }
+    }
+}
